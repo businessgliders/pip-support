8fe60841899689402b3c2511b1954a2128e77589
Pilates in Pink Studio <info@pilatesinpinkstudio.com>
2026-02-16 17:12:19 +0000

    File changes

--- functions/getAllUsers.ts
+++ functions/getAllUsers.ts
@@ -1,13 +1,14 @@
 export default async function getAllUsers(data, { base44 }) {
   try {
-    const users = await base44.asServiceRole.entities.User.list();
+    const users = await base44.asServiceRole.entities.User.list("-created_date", 100);
+    console.log('getAllUsers response:', { count: users.length, users });
     return {
-      success: true,
       users: users
     };
   } catch (error) {
+    console.error('getAllUsers error:', error);
     return {
-      success: false,
+      users: [],
       error: error.message
     };
   }
