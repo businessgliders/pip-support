7bb83c20fe7375a6501171f210a317605692c81e
Pilates in Pink Studio <info@pilatesinpinkstudio.com>
2026-02-16 04:45:05 +0000

    File changes

--- src/components/support/TicketCard.jsx
+++ src/components/support/TicketCard.jsx
@@ -17,14 +17,14 @@ const priorityBorderColors = {
 };
 
 const userColors = {
-  0: "bg-pink-300",
-  1: "bg-purple-300",
-  2: "bg-blue-300",
-  3: "bg-teal-300",
-  4: "bg-green-300",
-  5: "bg-amber-300",
-  6: "bg-rose-300",
-  7: "bg-indigo-300"
+  0: "bg-pink-400",
+  1: "bg-purple-400",
+  2: "bg-blue-400",
+  3: "bg-teal-400",
+  4: "bg-green-400",
+  5: "bg-amber-400",
+  6: "bg-rose-400",
+  7: "bg-indigo-400"
 };
 
 const inquiryTypeIcons = {
